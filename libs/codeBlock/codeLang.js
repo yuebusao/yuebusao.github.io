35e7d3846166923b4ce39d3d70fbef74232510fb
yuejianlong <1303516204@qq.com>
2022-11-15 11:25:25 +0800

    Site updated: 2022-11-15 11:25:25

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -1,4 +1,5 @@
 // 代码块语言识别
+
 $(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
 
@@ -13,7 +14,7 @@ $(function () {
 
     // 首字母大写
     // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-
+    
     $(this).siblings(".code_lang").text(lang_name);
   });
 });
